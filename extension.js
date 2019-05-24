17d8fa6489de2fde36ba8cb1089046b8e98848b2
cooperreid-optimizely <cooper.reid@optimizely.com>
2019-05-24 14:35:46 -0400

    move to analytics ext

--- extension.js
+++ extension.js
@@ -1,5 +1,5 @@
 window.__optCompositeTracker.setParams({
   composite_metric_api_name: extension.composite_metric_api_name,
   event_list: extension.event_list,
-  instance: extension.$instance
+  instance: 'exp-' + experimentId
 });
